43968290d1abaf49f17da0ed28e1d390acde4bac
Bhavya-mahyavanshi <bhavyamahyavanshi1534@gmail.com>
2025-05-04 15:05:24 -0400

    DSA Study plan

--- DSA Qestions Solution/RecursionAndBacktracking.java
+++ DSA Qestions Solution/RecursionAndBacktracking.java
@@ -51,18 +51,64 @@ public class RecursionAndBacktracking {
         TowerOfHanoi(num - 1, helper, src, dest);
     }
 
-    public static void subsets(int idx, String str, String ans){
-        if(idx == str.length()){
-            return;
-        }
-        ans = "";
-        for(int i = 0; i <=  idx; i++){
-            ans += str.charAt(i);
-        }
-        System.out.println(ans);
-        subsets(idx+1, str, ans);
+    public static void subsets(String str, int len, int idx, int arr[]){
+       if(idx == len){
+           for(int i = 0; i < len; i++){
+               if(arr[i] == 1){
+                   System.out.print(str.charAt(i));
+               }
+           }
+           System.out.println();
+           return;
+       }
+       arr[idx] = 1;
+       subsets(str, len, idx+ 1, arr);
+
+       arr[idx] = 0;
+       subsets(str, len, idx + 1, arr);
     }
 
+    public static void NQueens(int[][] board, int row, int n){
+        if(row == n){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; i < n; j++){
+                    System.out.println(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            if(isSafe(board, row, i, n)){
+                board[row][i] = 1;
+                NQueens(board, row+1, n);
+                board[row][i] = 0; //Backtrack
+            }
+        }
+    }
+
+    public static boolean isSafe(int[][] board, int row, int col, int n){
+        
+        for(int i = 0; i < row; i++){
+            if(board[i][col] == 1){
+                return false;
+            }
+        }
+        
+        for(int i = row, j = col; i >=0 && j >= 0; i--, j--){
+            if(board[i][j] == 1){
+                return false;
+            }
+        }
+       
+        for(int i = row, j = col; i >=0 && j < n; i--, j++){
+            if(board[i][j] == 1){
+                return false;
+            }
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
         //Print numbers from N to 1 using recursion. 
         Scanner sc = new Scanner(System.in);
@@ -89,7 +135,17 @@ public class RecursionAndBacktracking {
         //Generate all subsets of a given string.
         System.out.print("Enter Another String: ");
         String str1 = sc.next();
-        String ans = "";
-        subsets(0, str1, ans);
+        int len = str1.length();
+        int[] arr = new int[len];
+        subsets(str1, len, 0, arr);
+
+        //Solve N Queens problem using backtracking.
+        System.out.println("Enter the Number of Queens: ");
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        System.out.println("N Queens Problem: ");
+        NQueens(board,0, n);
+
+        sc.close();
     }
 }
